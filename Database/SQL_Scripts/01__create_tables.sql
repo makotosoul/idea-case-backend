febb399c7ee10cf3965d4af1f8c605bc8b319e72
valju <juhani.valimaki@haaga-helia.fi>
2022-09-07 13:28:56 +0300

    More edits to CREATE TABLES

--- Database/SQL_Scripts/01__create_tables.sql
+++ Database/SQL_Scripts/01__create_tables.sql
@@ -8,26 +8,27 @@ CREATE TABLE IF NOT EXISTS Department (
 ) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=latin1;
 
 CREATE TABLE IF NOT EXISTS `User` (
-  id            INTEGER         NOT NULL AUTO_INCREMENT,
-  name          VARCHAR(255)    UNIQUE NOT NULL,
-  email         VARCHAR(255)    UNIQUE NOT NULL,
-  isAdmin       BOOLEAN         NOT NULL,
+    id          INTEGER         NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(255)    UNIQUE NOT NULL,
+    email       VARCHAR(255)    UNIQUE NOT NULL,
+    isAdmin     BOOLEAN         NOT NULL,
 
-  PRIMARY KEY (id)
+    PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=201 DEFAULT CHARSET=latin1;
 
-CREATE TABLE IF NOT EXISTS `DepartmentPlanner` (
-  departmentId INTEGER,
-  userId INTEGER,
+CREATE TABLE IF NOT EXISTS DepartmentPlanner (
+    departmentId    INTEGER     NOT NULL,
+    userId          INTEGER     NOT NULL,
 
-  PRIMARY KEY (id),
-  CONSTRAINT FOREIGN KEY (departmentId) REFERENCES Department(id) 
-    ON DELETE CASCADE 
-    ON UPDATE NO ACTION,
-  CONSTRAINT FOREIGN KEY (userId) REFERENCES User(id) 
-    ON DELETE CASCADE 
-    ON UPDATE NO ACTION
-);
+    PRIMARY KEY (departmentId, userId),
+
+    CONSTRAINT FOREIGN KEY (departmentId) REFERENCES Department(id) 
+        ON DELETE CASCADE 
+        ON UPDATE NO ACTION,
+    CONSTRAINT FOREIGN KEY (userId) REFERENCES User(id) 
+        ON DELETE CASCADE 
+        ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE IF NOT EXISTS Campus (
     id          INTEGER         NOT NULL AUTO_INCREMENT,
@@ -35,88 +36,105 @@ CREATE TABLE IF NOT EXISTS Campus (
     description VARCHAR(16000)  NOT NULL,
 
     PRIMARY KEY (id)
-);
+) ENGINE=InnoDB AUTO_INCREMENT=301 DEFAULT CHARSET=latin1;
 
 CREATE TABLE IF NOT EXISTS Building (
-    id          INTEGER       NOT NULL AUTO_INCREMENT,
-    name        VARCHAR(255)     UNIQUE NOT NULL,
-    description VARCHAR(16000)    NOT NULL,
+    id          INTEGER         NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(255)    UNIQUE NOT NULL,
+    description VARCHAR(16000)  NOT NULL,
+    campusId    INTEGER         NOT NULL, 
 
     PRIMARY KEY (id)
-);
+) ENGINE=InnoDB AUTO_INCREMENT=401 DEFAULT CHARSET=latin1;
 
 CREATE TABLE IF NOT EXISTS Space (
-    id              INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id              INTEGER NOT NULL AUTO_INCREMENT,
     name            VARCHAR(255) NOT NULL,
-    area       SMALLINT UNSIGNED,
-    people_capasity SMALLINT UNSIGNED,
-    buildingId     INT UNSIGNED NOT NULL,
+    area            DECIMAL(5,1),
+    people_capasity INTEGER UNSIGNED,
+    buildingId      INTEGER NOT NULL,
     availableFrom   TIME,
     availableTo     TIME,
     classesFrom     TIME,
     classesTo       TIME,
 
-    PRIMARY KEY (`id`)
+    CONSTRAINT AK_UNIQUE_name_in_building UNIQUE(buildingId, name),
+
+    CONSTRAINT PK_Space PRIMARY KEY (`id`),
+
     CONSTRAINT FK_space_building
     	FOREIGN KEY (buildingId) REFERENCES Building(id)
     	ON DELETE CASCADE
     	ON UPDATE CASCADE
-)ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1001 DEFAULT CHARSET=latin1;
 
-CREATE TABLE IF NOT EXISTS `Equipment` (
-  `id` INTEGER NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255) DEFAULT NULL,
-  movable BOOLEAN,
-  `priority` INTEGER DEFAULT NULL,
-  description VARCHAR(16000)    NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS Equipment (
+    id            INTEGER             NOT NULL AUTO_INCREMENT,
+    name          VARCHAR(255)        UNIQUE NOT NULL,
+    isMovable       BOOLEAN             NOT NULL,
+    priority      INTEGER             NOT NULL DEFAULT 0,
+    description     VARCHAR(16000),
 
-CREATE TABLE IF NOT EXISTS `SpaceEquipment` (
-  `spaceId` INTEGER NOT NULL,
-  `equipmentId` INTEGER NOT NULL,
-  KEY `FK__space` (`spaceId`),
-  KEY `FK__equipment` (`equipmentId`),
-  CONSTRAINT `FK__equipment` FOREIGN KEY (`equipmentId`) REFERENCES `Equipment` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `FK__space` FOREIGN KEY (`spaceId`) REFERENCES `Space` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2001 DEFAULT CHARSET=latin1;
 
-CREATE TABLE IF NOT EXISTS `Program` (
-  id INTEGER NOT NULL AUTO_INCREMENT,
-  name VARCHAR(255) NOT NULL UNIQUE,
+CREATE TABLE IF NOT EXISTS SpaceEquipment (
+    spaceId       INTEGER     NOT NULL,
+    equipmentId   INTEGER     NOT NULL,
+    
+    CONSTRAINT PK_SpaceEquipment PRIMARY KEY(spaceId,equipmentId),
+
+    CONSTRAINT `FK_SpaceEquipment_Equipment` 
+        FOREIGN KEY (`equipmentId`) REFERENCES `Equipment` (`id`) 
+        ON DELETE NO ACTION ON UPDATE NO ACTION,
+    CONSTRAINT `FK_SpaceEquipment_Space` 
+        FOREIGN KEY (`spaceId`) REFERENCES `Space` (`id`) 
+        ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE = InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS Program (
+  id            INTEGER         NOT NULL AUTO_INCREMENT,
+  name          VARCHAR(255)    NOT NULL UNIQUE,
   -- participants INT(4),
-  departmentId INTEGER,
-  PRIMARY KEY (id),
-  CONSTRAINT FOREIGN KEY (departmentId) REFERENCES Department (id) 
-    ON DELETE NO ACTION 
-    ON UPDATE NO ACTION
-);
+  departmentId  INTEGER         NOT NULL,
 
-CREATE TABLE IF NOT EXISTS `Subject` (
-    `id`            INTEGER NOT NULL AUTO_INCREMENT,
-    name            VARCHAR(255),
+  PRIMARY KEY (id),
+  
+  CONSTRAINT FOREIGN KEY (departmentId) REFERENCES Department(id) 
+    ON DELETE NO ACTION       -- DNA
+    ON UPDATE NO ACTION       -- UNA
+) ENGINE=InnoDB AUTO_INCREMENT=3001 DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS Subject (
+    id              INTEGER         NOT NULL AUTO_INCREMENT,
+    name            VARCHAR(255)    UNIQUE,
     groupSize       INTEGER,
     groupCount      INTEGER,
     sessionLength   TIME,
     sessionCount    INTEGER,
-  `area` decimal(20,6) DEFAULT NULL,
-    `programId` INTEGER NOT NULL,
+    area            DECIMAL(5,1) DEFAULT NULL,         
+    programId       INTEGER NOT NULL,
+
+    CONSTRAINT AK_Subject_unique_name_in_program UNIQUE (programId, name),
   
-  PRIMARY KEY (`id`),
+    CONSTRAINT PK_Subject PRIMARY KEY (`id`),
 
-  CONSTRAINT `FK__program` FOREIGN KEY (`programId`) REFERENCES `Program` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+    CONSTRAINT `FK_Subject_Program` FOREIGN KEY (`programId`) 
+        REFERENCES `Program`(`id`) 
+        ON DELETE NO ACTION 
+        ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=4001 DEFAULT CHARSET=latin1;
 
-CREATE TABLE IF NOT EXISTS `SubjectEquipment` (
+CREATE TABLE IF NOT EXISTS SubjectEquipment (
     subjectId      INTEGER     NOT NULL,
     equipmentId    INTEGER     NOT NULL,
-    priority        INTEGER     NOT NULL,
+    priority       INTEGER     NOT NULL,
 
-    PRIMARY KEY (subjectId,equipmentId),
+    PRIMARY KEY (subjectId, equipmentId),
 
-    CONSTRAINT `FK_SubjectEquipment_Subject` FOREIGN KEY (`subjectId`) REFERENCES `Subject` (`id`) 
-        ON DELETE NO ACTION ON UPDATE NO ACTION
-    CONSTRAINT `FK_SubjectEquipment_Equipment` FOREIGN KEY (`equipmentId`) REFERENCES `Equipment` (`id`) 
+    CONSTRAINT `FK_SubjectEquipment_Subject` FOREIGN KEY (`subjectId`) REFERENCES `Subject`(`id`) 
         ON DELETE NO ACTION ON UPDATE NO ACTION,
-  
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+    CONSTRAINT `FK_SubjectEquipment_Equipment` FOREIGN KEY (`equipmentId`) REFERENCES `Equipment`(`id`) 
+        ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
