b090bc6a750ed406cef4d6179d34de074cc8668e
juhkiz <bgp040@myy.haaga-helia.fi>
2022-09-07 14:36:01 +0300

    First insert rows for Subject and SubjectEquipment

--- Database/SQL_Scripts/02__insert_test_data.sql
+++ Database/SQL_Scripts/02__insert_test_data.sql
@@ -20,7 +20,15 @@ USE casedb;
 /* --- Insert: Program * --- */
 
 /* --- Insert: Subject * --- */
-
+INSERT INTO Subject(name, groupSize, groupCount, sessionLength, sessionCount, area, programId) VALUES
+('Saksan kielen perusteet', 10, 2, '01:30:00', 2, 100.5, 3002),
+('Viulistien kehonhallinta', 20, 1, '00:30:00', 2, 200, 3003),
+('Piano yksilöopetus', 1, 1, '02:30:00', 2, 40, 3004);
 /* --- Insert: SubjectEquipment * --- */
+INSERT INTO SubjectEquipment(subjectId, equipmentId, priority) VALUES
+(4001, NULL, 5),
+(4002, NULL, 3),
+(4003, 2001, 1);
+
 
 /* END */
