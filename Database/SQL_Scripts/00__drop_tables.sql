8a760260f17c4c8130733dc63af06d626091f479
valju <juhani.valimaki@haaga-helia.fi>
2022-09-07 10:05:20 +0300

    More unification of DB scripts, not ready

--- Database/SQL_Scripts/00__drop_tables.sql
+++ Database/SQL_Scripts/00__drop_tables.sql
@@ -1,4 +1,17 @@
+-- DROP DATABASE IF EXISTS `casedb`;
+-- CREATE DATABASE IF NOT EXISTS `casedb`;
+
+USE `casedb`;
+
+
+DROP TABLE IF EXISTS Requirement;
+DROP TABLE IF EXISTS Subject;  -- "Jazz-rummunlyöjien yksilöopetus" 120ryhmää a 1, "Musiikin teoria", 3ryhmää a 20
+DROP TABLE IF EXISTS Program;  -- "Jazz-rummunlyöjä"
+DROP TABLE IF EXISTS `SpaceEquipment`;
+DROP TABLE IF EXISTS `Equipment`;  -- "Tuplaflyygeli"
+DROP TABLE IF EXISTS `Space`;
+DROP TABLE IF EXISTS Building;
+DROP TABLE IF EXISTS Campus;
 DROP TABLE IF EXISTS DepartmentPlanner;
-DROP TABLE IF EXISTS Program;
 DROP TABLE IF EXISTS `User`;
-DROP TABLE IF EXISTS Department;
+DROP TABLE IF EXISTS Department;  -- "Lyömäsoitinten aineryhmä"
