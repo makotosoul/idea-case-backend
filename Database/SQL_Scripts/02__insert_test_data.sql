c83f54076e893871199d515fdf41e8f3d058c29e
PutkisDude <putkis@gmail.com>
2022-09-07 15:54:37 +0300

    Add INSERT queries for Department, User, DepartmentPlanner, Program

--- Database/SQL_Scripts/02__insert_test_data.sql
+++ Database/SQL_Scripts/02__insert_test_data.sql
@@ -2,10 +2,31 @@ USE casedb;
 
 /* INSERTS */
 /* --- Insert: Department --- */
+INSERT INTO Department(name, description) VALUES
+	('Jazz', 'Aineryhmän kuvaus'),
+    ('Laulumusiikki', 'Aineryhmän kuvaus'),
+    ('Piano, harmonikka, kitara ja kantele', 'Aineryhmän kuvaus'),
+    ('Musiikkikasvatus', 'Aineryhmän kuvaus'),
+    ('MuTri', "Aineryhmän kuvaus"),
+    ('Vanha musiikki', "Aineryhmän kuvaus"),
+    ('Musiikkiteknologia', 'Aineryhmän kuvaus'),
+    ('Musiikinjohtaminen sekä orkesteri- ja kuorotoiminta', 'Aineryhmän kuvaus'),
+    ('Taidejohtaminen ja yrittäjyys', 'Aineryhmän kuvaus'),
+    ('Avoin Kampus', 'Aineryhmän kuvaus');
 
 /* --- Insert: `User` --- */
+INSERT INTO User(name, isAdmin, email) VALUES
+    ('sa1001', 1, 'fake_admin@test.co'),
+    ('sa1002', 0, 'fake_planner@test.co'),
+    ('sa1003', 0, 'fake_planner2@test.co');
 
 /* --- Insert: DepartmentPlanner * --- */
+INSERT INTO DepartmentPlanner(userId, departmentId) VALUES
+    (202, 101),
+    (202, 105),
+    (203, 103),
+    (203, 104),
+    (202, 102);
 
 /* --- Insert: Campus --- */
 
@@ -18,6 +39,17 @@ USE casedb;
 /* --- Insert: SpaceEquipment * --- */
 
 /* --- Insert: Program * --- */
+INSERT INTO Program (name , departmentId) VALUES
+    ('Piano', 103),
+    ('Laulutaide pääaineena', 102),
+    ('Kitara', 103),
+    ('Kantele', 103),
+    ('Jazzsävellys', 101),
+    ('Laulutaide', 102),
+    ('Musiikinteoria pääaineena', 104),
+    ('Jazzmusiikin instrumentti- tai lauluopinnot pääaineena', 102),
+    ('Fortepiano', 103),
+    ('Avoin Kampus', 110);
 
 /* --- Insert: Subject * --- */
 
