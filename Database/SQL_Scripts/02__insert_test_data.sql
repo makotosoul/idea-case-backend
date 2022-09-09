264ec2d6aec8be830b6827882bcda1058c480c3e
valju <juhani.valimaki@haaga-helia.fi>
2022-09-09 07:56:05 +0300

    Change table names follow standard: SpaceEquipment. Remove auto_incremented id:s from INSERTs

--- Database/SQL_Scripts/02__insert_test_data.sql
+++ Database/SQL_Scripts/02__insert_test_data.sql
@@ -29,25 +29,25 @@ INSERT INTO DepartmentPlanner(userId, departmentId) VALUES
     (202, 102);
 
 /* --- Insert: Campus --- */
-INSERT INTO `campus` (`id`, `name`, `description`) VALUES
-	(301, 'Siba', 'Sibeliusakatemian kampus');
+INSERT INTO `Campus` (`name`, `description`) VALUES
+	('Siba', 'Sibeliusakatemian kampus');
 /* --- Insert: Building * --- */
-INSERT INTO `building` (`id`, `name`, `description`, `campusId`) VALUES
-	(401, 'Musiikkitalo', 'Sibeliusakatemian päärakennus', 301),
-	(402, 'N-talo', 'Sibeliusakatemian opetus ja harjoittelu talo ', 301),
-	(403, 'R-talo', 'Sibeliusakatemian konserttitalo', 301);
+INSERT INTO `Building` (`name`, `description`, `campusId`) VALUES
+	('Musiikkitalo', 'Sibeliusakatemian päärakennus', 301),
+	('N-talo', 'Sibeliusakatemian opetus ja harjoittelu talo ', 301),
+	('R-talo', 'Sibeliusakatemian konserttitalo', 301);
 /* --- Insert: Space * --- */
-INSERT INTO `space` (`id`, `name`, `area`, `people_capasity`, `buildingId`, `availableFrom`, `availableTo`, `classesFrom`, `classesTo`) VALUES
-	(1001, 'S6117', 31.0, 10, 401, '08:00:00', '21:00:00', '09:00:00', '16:00:00'),
-	(1002, 'S6104 ', 63.0, 50, 401, '08:00:00', '21:00:00', '10:00:00', '17:00:00'),
-	(1003, 'R312', 15.0, 4, 403, '08:00:00', '21:00:00', '08:00:00', '18:00:00');
+INSERT INTO `Space` (`name`, `area`, `people_capasity`, `buildingId`, `availableFrom`, `availableTo`, `classesFrom`, `classesTo`) VALUES
+	('S6117', 31.0, 10, 401, '08:00:00', '21:00:00', '09:00:00', '16:00:00'),
+	('S6104 ', 63.0, 50, 401, '08:00:00', '21:00:00', '10:00:00', '17:00:00'),
+	('R312', 15.0, 4, 403, '08:00:00', '21:00:00', '08:00:00', '18:00:00');
 /* --- Insert: Equipment --- */
-INSERT INTO `equipment` (`id`, `name`, `isMovable`, `priority`, `description`) VALUES
-	(2001, 'Kontrabasso', 1, 110, 'Kaunis puinen soitin, jolla on neljä kieltä.'),
-	(2002, 'Urut', 0, 600, 'Valtavan kokoinen soitin'),
-	(2003, 'Kantele', 1, 50, 'Väinämöisen soitin');
+INSERT INTO `Equipment` (`name`, `isMovable`, `priority`, `description`) VALUES
+	('Kontrabasso', 1, 110, 'Kaunis puinen soitin, jolla on neljä kieltä.'),
+	('Urut', 0, 600, 'Valtavan kokoinen soitin'),
+	('Kantele', 1, 50, 'Väinämöisen soitin');
 /* --- Insert: SpaceEquipment * --- */
-INSERT INTO `spaceequipment` (`spaceId`, `equipmentId`) VALUES
+INSERT INTO `SpaceEquipment` (`spaceId`, `equipmentId`) VALUES
 	(1001, 2001),
 	(1002, 2002),
 	(1003, 2003);
