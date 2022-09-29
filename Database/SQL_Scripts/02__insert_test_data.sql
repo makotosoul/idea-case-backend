054e2e9823111d614ef1e9135f7af81ac72473dd
elinaelkama <elina.elkama@hotmail.com>
2022-09-29 16:21:06 +0300

    add, alter testdata

--- Database/SQL_Scripts/02__insert_test_data.sql
+++ Database/SQL_Scripts/02__insert_test_data.sql
@@ -16,7 +16,7 @@ INSERT INTO Department(name, description) VALUES
     ('Musiikkiteknologia', 'Aineryhmän kuvaus'),
     ('Musiikinjohtaminen sekä orkesteri- ja kuorotoiminta', 'Aineryhmän kuvaus'),
     ('Taidejohtaminen ja yrittäjyys', 'Aineryhmän kuvaus'),
-    ('DocMus', 'Aineryhmän kuvaus'),
+    ('DocMus', 'Tohtoritason koulutusohjelma'),
     ('Kansanmusiikki ja Global music & GLOMAS', 'Aineryhmän kuvaus'),
     ('Kirkkomusiikki ja urut', 'Aineryhmän kuvaus'),
     ('Jouset ja kamarimusiikki', 'Aineryhmän kuvaus'),
@@ -41,6 +41,7 @@ INSERT INTO DepartmentPlanner(userId, departmentId) VALUES
 /* --- Insert: Campus --- */
 INSERT INTO `Campus` (`name`, `description`) VALUES
 	('Siba', 'Sibeliusakatemian kampus');
+    
 /* --- Insert: Building * --- */
 INSERT INTO `Building` (`name`, `description`, `campusId`) VALUES
 	('Musiikkitalo', 'Sibeliusakatemian päärakennus', 301),
@@ -55,17 +56,40 @@ INSERT INTO SubjectType(name) VALUES
     ('Ryhmäsoitto');
 
 /* --- Insert: `Space` * --- */
-INSERT INTO `Space` (`name`, `area`, `people_capasity`, `buildingId`, `availableFrom`, `availableTo`, `classesFrom`, `classesTo`, `subjectTypeId`) VALUES
-	('S6117', 31.0, 10, 401, '08:00:00', '21:00:00', '09:00:00', '16:00:00', 801),
-	('S6104 ', 63.0, 50, 401, '08:00:00', '21:00:00', '10:00:00', '17:00:00', 804),
-	('R312', 15.0, 4, 403, '08:00:00', '21:00:00', '08:00:00', '18:00:00', 802);
+INSERT INTO `Space` (`name`, `area`, `people_capasity`, `buildingId`, `availableFrom`, `availableTo`, `classesFrom`, `classesTo`, `subjectTypeId`, `info`) VALUES
+	('S6117 Jouset/Kontrabasso', 31.9, 7, 401, '08:00:00', '21:00:00', '09:00:00', '16:00:00', 801, 'ONLY FOR BASSISTS'),
+	('S6104 Didaktiikkaluokka Inkeri', 62.5, 30, 401, '08:00:00', '21:00:00', '10:00:00', '17:00:00', 804, 'Musiikkikasvatus'),
+	('S7106 Kansanmusiikki/AOV', 63.7, 22, 401, '08:00:00', '21:00:00', '08:00:00', '18:00:00', 802, 'Yhtyeluokka'), 
+    ('S6114 Perkussioluokka/Marimbaluokka', 33.3, 4, 401, '08:00:00', '22:00:00', '09:00:00', '15:00:00', 802, 'Vain lyömäsoittajat'),
+    ('S1111 Studio Erkki', 36.0, 15, 401, '08:00:00', '22:00:00', '11:00:00', '15:00:00', 802, 'Tilatyyppi: Studio'),
+    ('S5109 Jazz/Bändiluokka', 17.5, 2, 401, '08:00:00', '20:00:00', '08:00:00', '16:00:00', 802, 'ONLY FOR JAZZ DEPARTMENT'),
+    ('S6112 Harppuluokka', 28.8, 4, 401, '08:00:00', '17:00:00', '11:00:00', '16:00:00', 802, 'Vain harpistit'),
+    ('S6113 Puhaltimet/Klarinetti/Harppu', 18.1, 4, 401, '08:00:00', '19:00:00', '08:00:00', '19:00:00', 802, 'Tilatyyppi: Musiikkiluokka'),
+    
+    ('R312 Opetusluokka', 16.6, 6, 403, '08:00:00', '21:00:00', '08:00:00', '18:00:00', 802, 'Tilatyyppi: Musiikkiluokka'), 
+    ('R530 Opetusluokka', 50.0, 18, 403, '08:00:00', '21:00:00', '08:00:00', '19:00:00', 802, 'Luentoluokka'),
+    ('R213 Harjoitushuone', 20.0, 4, 403, '08:00:00', '21:00:00', '10:00:00', '16:00:00', 802, 'Ensisijainen varausoikeus vanhan musiikin aineryhmällä'),
+    ('R510 Opetusluokka', 81.0, 30, 403, '08:00:00', '21:00:00', '09:00:00', '15:00:00', 802, 'Luentoluokka'),
+    ('R416 Opetusluokka', 23.0, 9, 403, '08:00:00', '21:00:00', '10:00:00', '17:00:00', 802, 'Tilatyyppi: Musiikkiluokka'),
+    ('R422 Opetusluokka', 23.0, 11, 403, '08:00:00', '19:00:00', '08:00:00', '22:00:00', 802, 'Kitara'),
+    ('R410 Opetusluokka', 42.4, 20, 403, '08:00:00', '19:00:00', '08:00:00', '20:00:00', 802, 'Pianopedagogiikka'),
+    ('R531 Opetusluokka', 53.0, 17, 403, '09:00:00', '20:00:00', '10:00:00', '14:00:00', 802, 'Tilatyyppi: Musiikkiluokka'),
 
+    ('N522 Säestysluokka', 33.0, 8, 402, '08:00:00', '21:00:00', '08:00:00', '19:00:00', 802, 'Tilatyyppi: Musiikkiluokka'),
+    ('N319 Jazz/Lyomäsoittimet, piano ja yhtyeet', 34.0, 5, 402, '08:00:00', '19:00:00', '08:00:00', '17:00:00', 802, 'Varaukset Jukkis Uotilan kautta'),
+    ('N315 Jouset', 15.5, 4, 402, '08:00:00', '21:00:00', '08:00:00', '14:00:00', 802, 'Tilatyyppi: Musiikkiluokka'),
+    ('N419 Kirkkomusiikki/Urkuluokka', 34.0, 5, 402, '09:00:00', '20:00:00', '08:00:00', '18:00:00', 802, 'Tilatyyppi: Musiikkiluokka'),
+    ('N517 Opetusluokka', 15.5, 3, 402, '08:00:00', '21:00:00', '08:00:00', '15:00:00', 802, 'Tilatyyppi: Musiikkiluokka'),
+    ('N425 Jouset/Sello', 33.0, 8, 402, '08:00:00', '22:00:00', '09:00:00', '15:00:00', 802, 'Tilatyyppi: Musiikkiluokka'),
+    ('N312 Musiikkikasvatus/Vapaasäestys', 34.0, 8, 402, '08:00:00', '22:00:00', '08:00:00', '15:00:00', 802, 'Tilatyyppi: Musiikkiluokka'),
+    ('N220 Tohtorikoulut', 49.5, 20, 402, '08:00:00', '19:00:00', '08:00:00', '17:00:00', 802, 'Tilatyyppi: Luentoluokka');
+    
 /* --- Insert: Equipment --- */
 INSERT INTO `Equipment` (`name`, `isMovable`, `priority`, `description`) VALUES
 	('Urut', 0, 600, 'Valtavan kokoinen soitin'),
 	('Kantele', 1, 50, 'Väinämöisen soitin'),
     ('Nokkahuilu', 1, 50, 'Kaikki rakastaa'),
-    ('Rumpusetti, Jazz', 1, 250, 'Ääntä riittää'),
+    ('Rumpusetti', 1, 250, 'Ääntä riittää'),
     ('Äänityslaitteisto Xyz', 0, 900, '8 kanavaa'),
     ('Viulu', 1, 50, 'Jousisoitin, 4-kieltä'),
     ('Alttoviulu', 1, 50, 'Jousisoitin, suurempi kuin viulu'),
@@ -77,13 +101,46 @@ INSERT INTO `Equipment` (`name`, `isMovable`, `priority`, `description`) VALUES
     ('Fortepiano', 0, 500, 'Pianon varhaismuoto'),
     ('Huilu', 1, 50, 'puhallinsoitin'),
     ('Oboe', 1, 100, 'puupuhallinsoitin'),
-    ('Tuuba', 1, 100, 'Suurehko puhallinsoitin');
+    ('Tuuba', 1, 100, 'Suurehko puhallinsoitin'),
+    ('Trumpetti', 1, 50, 'Puhallinsoitin'),
+    ('Clavinova', 1, 100, 'Sähköpiano'),
+    ('Bassovahvistin', 1, 50, 'Boom boom'),
+    ('Kitaravahvistin', 1, 50, 'Äänekäs laatikko'),
+    ('Flyygeli', 1, 200, ''),
+    ('DVD-soitin', 1, 50, ''),
+    ('Äänentoisto (ei PA-laitteet)', 0, 100, ''),
+    ('Näyttölaite (videoprojektori)', 0, 200, ''),
+    ('Yhtyeluokan äänentoisto', 0, 300, 'PA-laitteet'),
+    ('Dokumenttikamera', 0, 250, '');
     
 /* --- Insert: SpaceEquipment * --- */
 INSERT INTO `SpaceEquipment` (`spaceId`, `equipmentId`) VALUES
-	(1001, 2001),
-	(1002, 2002),
-	(1003, 2003);
+	(1001, 2021),
+    (1001, 2022),
+    (1001, 2023),
+    (1001, 2024),
+	(1002, 2021),
+    (1002, 2004),
+    (1002, 2019),
+    (1002, 2020),
+    (1002, 2022),
+    (1002, 2023),
+    (1002, 2025),
+    (1002, 2024),
+    (1002, 2026),
+	(1009, 2010),
+    (1009, 2021),
+    (1009, 2023),
+    (1013, 2021),
+    (1013, 2023),
+    (1020, 2001),
+    (1020, 2022),
+    (1020, 2023),
+    (1020, 2024),
+    (1019, 2021),
+    (1019, 2022),
+    (1019, 2023),
+    (1019, 2024);
     
 /* --- Insert: Program * --- */
 INSERT INTO Program (name , departmentId) VALUES
@@ -139,17 +196,28 @@ INSERT INTO Subject(name, groupSize, groupCount, sessionLength, sessionCount, ar
 ('Viulunsoitto, taso D', 1, 12, '01:00:00', 1, 10, 3013, 802 ),
 ('Tuubansoitto, taso C', 1, 5, '01:00:00', 1, 15, 3026, 802 ),
 ('Harmonikansoitto (musiikin kandidaatti)', 1, 2, '01:00:00', 1, 15, 3011, 802),
-('Jazz, rumpujensoitto, taso B', 1, 4, '01:00:00', 1, 15, 3017, 802);
+('Jazz, rumpujensoitto, taso B', 1, 4, '01:00:00', 1, 15, 3017, 802),
+('Kansanmusiikkiteoria 1', 1, 20, '01:00:00', 2, 40, 3014, 801);
 
 /* --- Insert: SubjectEquipment * --- */
 INSERT INTO SubjectEquipment(subjectId, equipmentId, priority) VALUES
-(4001, 2001, 90),
-(4001, 2002, 900),
-(4001, 2003, 90),
-(4001, 2004, 40),
-(4005, 2005, 170),
-(4006, 2001, 90),
-(4007, 2006, 900);
+(4003, 2021, 900),
+(4004, 2017, 50),
+(4005, 2001, 900),
+(4007, 2011, 100),
+(4008, 2009, 90),
+(4009, 2002, 50),
+(4010, 2002, 90),
+(4011, 2001, 900),
+(4012, 2011, 90),
+(4013, 2012, 50),
+(4014, 2013, 900),
+(4015, 2003, 50),
+(4016, 2006, 90),
+(4017, 2016, 90),
+(4018, 2012, 90),
+(4019, 2014, 800),
+(4020, 2010, 400);
 
 /* --- Insert: AllocRound * --- */
 INSERT INTO AllocRound(name, isSeasonAlloc, userId) VALUES
