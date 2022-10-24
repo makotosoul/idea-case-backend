5a90f6fc1c17c21a831a348fefedc79b4061d611
Janita <bgj369@myy.haaga-helia.fi>
2022-10-24 11:45:45 +0300

    Validation korjattu

--- validationHandler/index.js
+++ validationHandler/index.js
@@ -5,7 +5,7 @@ let validateAddUpdateSubject = [
     .isLength({ min: 2, max: 255 })
     .withMessage("Must be between 2-255 characters long")
     .bail()
-    .matches(/^[A-Za-zäöåÄÖÅ-\s]*$/) // /^[A-Z/i]+[a-z\u00c0-\u017e_ ]{2,255}$/)
+    .matches(/[A-Za-zäöåÄÖÅ0-9-\s]*$/) // /^[A-Z/i]+[a-z\u00c0-\u017e_ ]{2,255}$/)
     .withMessage("Must contain only letters")
     .bail()
     .notEmpty()
@@ -26,7 +26,8 @@ let validateAddUpdateSubject = [
     .withMessage("Cannot be empty")
     .bail(),
   check("sessionLength")
-    .matches(/^(?:\d|[01]\d|2[0-3]):[0-5]\d$/)
+    // .matches(/^(?:\d|[01]\d|2[0-3]):[0-5]\d$/)
+    .matches(/^([0-1][0-9]|[2][0-3]):([0-5][0-9]):([0-5][0-9])$/)
     .withMessage("Accepted format: 00:00")
     .bail()
     .notEmpty()
