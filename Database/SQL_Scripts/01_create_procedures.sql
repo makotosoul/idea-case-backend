371b693cc92e4c9ef2d2ae4532f8c95d7b9e9656
PutkisDude <putkis@gmail.com>
2022-11-17 19:16:06 +0200

    Fix multiple bugs in allocatespace procedure and update 000 create sql

--- Database/SQL_Scripts/01_create_procedures.sql
+++ Database/SQL_Scripts/01_create_procedures.sql
@@ -14,6 +14,7 @@ BEGIN
 	DECLARE allocated INTEGER DEFAULT 0; -- How many sessions added to AllocSpace
 	DECLARE sessionSeconds INTEGER DEFAULT 0; -- How many seconds each session lasts
 	DECLARE noSuitableSpace BOOLEAN DEFAULT TRUE; -- If can't allocate set false
+	DECLARE loopOn BOOLEAN DEFAULT TRUE;
 
 	SET sessions := (SELECT groupCount * sessionCount FROM Subject WHERE id = subId);
    	SET allocated := 0; -- How many sessions allocated
@@ -28,7 +29,7 @@ BEGIN
 		SET noSuitableSpace := FALSE;
    	ELSE -- Find for each session space with free time
    		SET i := 0;
-   		WHILE (allocated < sessions) OR (i < sessions - allocated) DO -- Try add all subject sessions to spaces	
+   		WHILE loopOn DO -- Try add all subject sessions to spaces	
    			SET spaceTo := (
    				SELECT sp.id FROM AllocSubjectSuitableSpace ass
 				LEFT JOIN Space sp ON ass.spaceId = sp.id
@@ -44,19 +45,25 @@ BEGIN
 			
 			IF spaceTo IS NULL THEN -- If can't find space with freetime for specific amount sessions
 				SET i := i+1;
+				IF i = sessions - allocated THEN
+					SET loopOn = FALSE;
+				END IF;
 			ELSE -- if can find space with freetime for specific amount sessions
-				INSERT INTO AllocSpace
+			INSERT INTO AllocSpace
 					(subjectId, allocRound, spaceId, totalTime) 
 				VALUES 
 					(subId, allocRouId, spaceTo, SEC_TO_TIME((sessionSeconds * (sessions - i - allocated))))
 				ON DUPLICATE KEY UPDATE totalTime = totalTime + SEC_TO_TIME(sessionSeconds * (sessions - i - allocated));
 				
-				SET i := 0;
 				SET allocated := allocated + (sessions - i - allocated);
+				SET i := 0;
+				IF allocated = sessions THEN
+					SET loopOn = FALSE;
+				END IF;
 			END IF;
    			
    		END WHILE;
-   
+   	
    END IF;
    
    IF sessions = allocated THEN -- If all sessions allocated
@@ -64,28 +71,24 @@ BEGIN
    ELSEIF noSuitableSpace = FALSE THEN -- if can't find any suitable space for the subject
    	UPDATE AllocSubject SET cantAllocate = 1 WHERE subjectId = subId AND allocRound = allocRouId;
    ELSEIF allocated = 0 AND noSuitableSpace = TRUE THEN -- if can't find any space with free time, add all sessions to same space with most freetime
-   	SET spaceTo := (SELECT spa.id
+   SET spaceTo := (SELECT spa.id
 					FROM AllocSubjectSuitableSpace suitspace
-					LEFT JOIN Space spa ON suitspace.spaceId = sp.id
+					LEFT JOIN Space spa ON suitspace.spaceId = spa.id
 					LEFT JOIN AllocSpace allspa ON suitspace.spaceId = allspa.spaceId
 					WHERE suitspace.subjectId = subId
 					AND suitspace.missingItems = 0
 					AND suitspace.allocRound = allocRouId
 					GROUP BY suitspace.spaceId 
-					ORDER BY (TIME_TO_SEC(TIMEDIFF(spa.availableTO, spa.availableFrom)) *5) - IFNULL((SUM(TIME_TO_SEC(allspa.totalTime))), 0) DESC
+					ORDER BY (TIME_TO_SEC(TIMEDIFF(spa.availableTO, spa.availableFrom)) *5) - IFNULL((SUM(TIME_TO_SEC(allspa.totalTime))), 0) ASC
 					LIMIT 1);
 				
    	INSERT INTO AllocSpace (subjectId, allocRound, spaceId, totalTime) 
    		VALUES (subId, allocRouId, spaceTo, SEC_TO_TIME(sessionSeconds * sessions)); -- NOT TESTED - NO ENOUGH TEST DATA ATM
    
    	ELSE -- when there is no enough freetime for all sessions in spaces, add rest to same space with others
-   		SET spaceTo := (SELECT * FROM
-						AllocSpace
-						WHERE subjectId = subId
-						AND allocRound = allocRouId
-						ORDER BY totalTime DESC);
+   		SET spaceTo := (SELECT spaceId FROM AllocSpace WHERE subjectId = subId AND allocRound = allocRouId ORDER BY totalTime ASC LIMIT 1);
 		
-		UPDATE AllocSpace SET totalTime=totalTime + (SEC_TO_TIME(sessionSeconds * (sessions - allocated))) 
+		UPDATE AllocSpace SET totalTime=ADDTIME(totalTime,(SEC_TO_TIME(sessionSeconds * (sessions - allocated))))
 		WHERE subjectId=subID AND spaceId = spaceTO AND allocRound = allocRouId;
    	
 		UPDATE AllocSubject SET isAllocated = 1 WHERE subjectId = subId AND allocRound = allocRouId;
@@ -95,6 +98,7 @@ BEGIN
 END; //
 
 DELIMITER ;
+
 
 /* Reset allocation */
 DELIMITER //
--- Database/SQL_Scripts/protoilua/allocatespace.sql
+++ Database/SQL_Scripts/protoilua/allocatespace.sql
@@ -1,4 +1,3 @@
-DROP PROCEDURE IF EXISTS allocateSpace;
 DELIMITER //
 
 CREATE PROCEDURE allocateSpace(allocRouId INT, subId INT) 
@@ -9,6 +8,7 @@ BEGIN
 	DECLARE allocated INTEGER DEFAULT 0; -- How many sessions added to AllocSpace
 	DECLARE sessionSeconds INTEGER DEFAULT 0; -- How many seconds each session lasts
 	DECLARE noSuitableSpace BOOLEAN DEFAULT TRUE; -- If can't allocate set false
+	DECLARE loopOn BOOLEAN DEFAULT TRUE;
 
 	SET sessions := (SELECT groupCount * sessionCount FROM Subject WHERE id = subId);
    	SET allocated := 0; -- How many sessions allocated
@@ -23,7 +23,7 @@ BEGIN
 		SET noSuitableSpace := FALSE;
    	ELSE -- Find for each session space with free time
    		SET i := 0;
-   		WHILE (allocated < sessions) OR (i < sessions - allocated) DO -- Try add all subject sessions to spaces	
+   		WHILE loopOn DO -- Try add all subject sessions to spaces	
    			SET spaceTo := (
    				SELECT sp.id FROM AllocSubjectSuitableSpace ass
 				LEFT JOIN Space sp ON ass.spaceId = sp.id
@@ -39,19 +39,25 @@ BEGIN
 			
 			IF spaceTo IS NULL THEN -- If can't find space with freetime for specific amount sessions
 				SET i := i+1;
+				IF i = sessions - allocated THEN
+					SET loopOn = FALSE;
+				END IF;
 			ELSE -- if can find space with freetime for specific amount sessions
-				INSERT INTO AllocSpace
+			INSERT INTO AllocSpace
 					(subjectId, allocRound, spaceId, totalTime) 
 				VALUES 
 					(subId, allocRouId, spaceTo, SEC_TO_TIME((sessionSeconds * (sessions - i - allocated))))
 				ON DUPLICATE KEY UPDATE totalTime = totalTime + SEC_TO_TIME(sessionSeconds * (sessions - i - allocated));
 				
-				SET i := 0;
 				SET allocated := allocated + (sessions - i - allocated);
+				SET i := 0;
+				IF allocated = sessions THEN
+					SET loopOn = FALSE;
+				END IF;
 			END IF;
    			
    		END WHILE;
-   
+   	
    END IF;
    
    IF sessions = allocated THEN -- If all sessions allocated
@@ -59,28 +65,24 @@ BEGIN
    ELSEIF noSuitableSpace = FALSE THEN -- if can't find any suitable space for the subject
    	UPDATE AllocSubject SET cantAllocate = 1 WHERE subjectId = subId AND allocRound = allocRouId;
    ELSEIF allocated = 0 AND noSuitableSpace = TRUE THEN -- if can't find any space with free time, add all sessions to same space with most freetime
-   	SET spaceTo := (SELECT spa.id
+   SET spaceTo := (SELECT spa.id
 					FROM AllocSubjectSuitableSpace suitspace
-					LEFT JOIN Space spa ON suitspace.spaceId = sp.id
+					LEFT JOIN Space spa ON suitspace.spaceId = spa.id
 					LEFT JOIN AllocSpace allspa ON suitspace.spaceId = allspa.spaceId
 					WHERE suitspace.subjectId = subId
 					AND suitspace.missingItems = 0
 					AND suitspace.allocRound = allocRouId
 					GROUP BY suitspace.spaceId 
-					ORDER BY (TIME_TO_SEC(TIMEDIFF(spa.availableTO, spa.availableFrom)) *5) - IFNULL((SUM(TIME_TO_SEC(allspa.totalTime))), 0) DESC
+					ORDER BY (TIME_TO_SEC(TIMEDIFF(spa.availableTO, spa.availableFrom)) *5) - IFNULL((SUM(TIME_TO_SEC(allspa.totalTime))), 0) ASC
 					LIMIT 1);
 				
    	INSERT INTO AllocSpace (subjectId, allocRound, spaceId, totalTime) 
    		VALUES (subId, allocRouId, spaceTo, SEC_TO_TIME(sessionSeconds * sessions)); -- NOT TESTED - NO ENOUGH TEST DATA ATM
    
    	ELSE -- when there is no enough freetime for all sessions in spaces, add rest to same space with others
-   		SET spaceTo := (SELECT * FROM
-						AllocSpace
-						WHERE subjectId = subId
-						AND allocRound = allocRouId
-						ORDER BY totalTime DESC);
+   		SET spaceTo := (SELECT spaceId FROM AllocSpace WHERE subjectId = subId AND allocRound = allocRouId ORDER BY totalTime ASC LIMIT 1);
 		
-		UPDATE AllocSpace SET totalTime=totalTime + (SEC_TO_TIME(sessionSeconds * (sessions - allocated))) 
+		UPDATE AllocSpace SET totalTime=ADDTIME(totalTime,(SEC_TO_TIME(sessionSeconds * (sessions - allocated))))
 		WHERE subjectId=subID AND spaceId = spaceTO AND allocRound = allocRouId;
    	
 		UPDATE AllocSubject SET isAllocated = 1 WHERE subjectId = subId AND allocRound = allocRouId;
