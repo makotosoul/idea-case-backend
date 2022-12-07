317d1b583763900d7e3290eb97159635aeb34945
PutkisDude <putkis@gmail.com>
2022-12-07 22:20:32 +0200

    Allocation reset blocked if allocation in progress

--- Database/SQL_Scripts/protoilua/resetAllocation.sql
+++ Database/SQL_Scripts/protoilua/resetAllocation.sql
@@ -3,6 +3,23 @@ DELIMITER //
 
 CREATE PROCEDURE IF NOT EXISTS  resetAllocation(allocR INTEGER)
 BEGIN
+	
+	-- Handler for the error
+	DECLARE processBusy CONDITION FOR SQLSTATE '50000';
+	DECLARE EXIT HANDLER FOR processBusy
+	BEGIN
+		GET DIAGNOSTICS CONDITION 1 @sqlstate = RETURNED_SQLSTATE, @errno = MYSQL_ERRNO, @text = MESSAGE_TEXT;
+		SET @full_error = CONCAT("Error: ", @errno, " (", @sqlstate, "): ", @text);
+		RESIGNAL SET MESSAGE_TEXT = @full_error;
+	END;
+	-- Raise error if allocation in progress.
+	SET @procedure_active = (SELECT processOn FROM AllocRound WHERE id = allocR);
+	IF @procedure_active = 1 THEN
+		SET @message_text = CONCAT("The allocation with allocRound:", allocR, " is currently in progress.");
+		SIGNAL processBusy SET MESSAGE_TEXT = @message_text, MYSQL_ERRNO = 1192;
+	END IF;
+	
+	-- Delete all allocation data and reset variables
 	DELETE FROM AllocSpace WHERE allocRound = allocR;
 	DELETE FROM AllocSubjectSuitableSpace WHERE allocRound = allocR;
     IF (allocR = 10004) THEN
