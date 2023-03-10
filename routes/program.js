92a77e17aa7cc3eb15c7239d80559741bd85ba8d
bgu303 <bgu303@myy.haaga-helia.fi>
2023-03-10 15:33:58 +0200

    Equipment backend first endpoints

--- routes/program.js
+++ routes/program.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import db from '../db/index.js';
+import db_knex from '../db/index_knex.js';
 import { dbErrorHandler, successHandler } from '../responseHandler/index.js';
 
 const program = express.Router();
@@ -15,5 +16,17 @@ program.get("/getSelectData", (req, res) => {
     }
   });
 });
+
+
+program.get("/:id", (req, res) => {
+  db_knex("Program").select().where("id", req.params.id)
+  .then(data => {
+    successHandler(res, data, "Succesfully read the programs from DB");
+  })
+  .catch(err => {
+    dbErrorHandler(res, err, "Oops! Nothing came through - Program");
+  })
+})
+
 
 export default program;
