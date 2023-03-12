00281f0c8734ae1d422737c71c4d41bc573fa610
Amaru Korpimäki <amarukorpimaki@Amarus-MBP.home>
2023-03-12 18:01:16 +0200

    WIP department crudl delete

--- routes/department.js
+++ routes/department.js
@@ -1,6 +1,7 @@
 import express from "express";
 import db_knex from "../db/index_knex.js";
 import {
+	dbErrorHandler,
 	requestErrorHandler,
 	successHandler,
 } from "../responseHandler/index.js";
@@ -15,6 +16,26 @@ department.get("/getDeptData", (req, res) => {
 		})
 		.catch((err) => {
 			requestErrorHandler(res, err, "Oops! Nothing came through - Department");
+		});
+});
+
+department.delete("/:id", (req, res) => {
+	db_knex("Department")
+		.where("id", req.params.id)
+		.del()
+		.then((rowsAffected) => {
+			if (rowsAffected === 1) {
+				successHandler(
+					res,
+					rowsAffected,
+					`Delete succesful! Count of deleted rows: ${rowsAffected}`
+				);
+			} else {
+				requestErrorHandler(res, `Invalid number id: ${req.params.id}`);
+			}
+		})
+		.catch((error) => {
+			dbErrorHandler(res, error);
 		});
 });
 
