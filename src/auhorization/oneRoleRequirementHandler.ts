1ae2a0c649cbe258125cb5cdd248c0c1d533e3e0
valju <juhani.valimaki@haaga-helia.fi>
2023-04-24 13:09:21 +0300

    two fixes

--- src/auhorization/oneRoleRequirementHandler.ts
+++ src/auhorization/oneRoleRequirementHandler.ts
@@ -7,9 +7,10 @@ export const oneRoleRequirementHandler = (
   roleName: string,
 ) => {
   req.requiredRolesList.push(roleName);
-  let rolePropertyName = `is${roleName
-    .substring(0, 1)
-    .toUpperCase()}${roleName.substring(1)}`;
+  let rolePropertyName = `is${roleName.substring(0, 1).toUpperCase()}${roleName
+    .substring(1)
+    .toLowerCase()}`;
+  // e.g. isAdmin
   if (req.user[rolePropertyName] === 1) {
     req.areRolesRequired = 1;
     console.log(`Logged in User has role: ${roleName}`);
@@ -17,7 +18,7 @@ export const oneRoleRequirementHandler = (
     if (req.areRolesRequired === 0) {
       req.areRolesRequired = -1;
     } else {
-      // req.areRolesRequired must have already been 1
+      // req.areRolesRequired must have already been 1 or -1
     }
   }
 
