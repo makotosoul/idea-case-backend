0e5e315068760c4ec5c99dd1141a96f6ee8d2b42
valju <juhani.valimaki@haaga-helia.fi>
2023-09-06 13:43:03 +0300

    One 'shortcut return' provided a if-else for clarity

--- src/authorization/userValidation.ts
+++ src/authorization/userValidation.ts
@@ -17,28 +17,28 @@ export const authenticator = (req: any, res: Response, next: any) => {
     logger.error(`Token: ${token}`);
     authenticationErrorHandler(req, res, 'Login TOKEN not found in headers');
     return;
-  }
+  } else {
+    try {
+      const verified = jsonwebtoken.verify(
+        token,
+        process.env.SECRET_TOKEN as string,
+      ) as JwtPayload | string;
+      const currentTime = Math.floor(Date.now() / 1000); // Time in seconds
+      const iat = typeof verified === 'object' ? verified.iat : 0;
 
-  try {
-    const verified = jsonwebtoken.verify(
-      token,
-      process.env.SECRET_TOKEN as string,
-    ) as JwtPayload | string;
-    const currentTime = Math.floor(Date.now() / 1000); // Time in seconds
-    const iat = typeof verified === 'object' ? verified.iat : 0;
+      // Checking if token is older than 1 hour (3600 seconds)
+      if (currentTime - (iat || 0) > 3600) {
+        authenticationErrorHandler(req, res, 'Token Expired');
+        return;
+      }
 
-    // Checking if token is older than 1 hour (3600 seconds)
-    if (currentTime - (iat || 0) > 3600) {
-      authenticationErrorHandler(req, res, 'Token Expired');
-      return;
+      req.user = verified;
+      req.areRolesRequired = 0;
+      // 0:none required, -1:at least one required, 1: role need satisfied
+      req.requiredRolesList = [];
+      next();
+    } catch (err) {
+      authenticationErrorHandler(req, res, 'Login token found but NOT valid');
     }
-
-    req.user = verified;
-    req.areRolesRequired = 0;
-    // 0:none required, -1:at least one required, 1: role need satisfied
-    req.requiredRolesList = [];
-    next();
-  } catch (err) {
-    authenticationErrorHandler(req, res, 'Login token found but NOT valid');
   }
 };
