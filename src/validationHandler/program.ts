60f790b83d2ee190bb56e7683295e17b7f1d4934
Suraj Mishra <mishrasur7@gmail.com>
2023-09-27 13:55:45 +0300

    basic validation added to create program route

--- src/validationHandler/program.ts
+++ src/validationHandler/program.ts
@@ -1,4 +1,4 @@
 import { validateIdObl, validateNameObl } from './index.js';
 
 //this needs to continue
-const validateProgram = [...validateNameObl, ...validateIdObl];
+export const validateProgram = [...validateNameObl, ...validateIdObl];
