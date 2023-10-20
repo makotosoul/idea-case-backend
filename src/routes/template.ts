a2e5107d137e3c891e7af5ba38e84c03cfe83f27
Suraj Mishra <mishrasur7@gmail.com>
2023-10-20 15:15:42 +0300

    template route modified

--- src/routes/template.ts
+++ src/routes/template.ts
@@ -3,7 +3,13 @@ import express from 'express';
 const template = express.Router();
 
 template.get('/', (req, res) => {
-  res.send('../../tempates/building_template.xlsx');
+  res
+    .setHeader(
+      'Content-Type',
+      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    )
+    .setHeader('Content-Disposition', 'attachment; filename=templaatti.xlsx')
+    .download('./tempates/building_template.xlsx', 'building_templaatti.xlsx');
 });
 
 export default template;
