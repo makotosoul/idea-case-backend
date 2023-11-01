efd3a7783e14c167273730887297f85c4df4ee85
nicklasstellberg <nicklas.stellberg@hotmail.com>
2023-11-01 12:04:17 +0200

    Edit for space and validation

---
+++ src/validationHandler/space.ts
@@ -0,0 +1,27 @@
+import { body, check } from 'express-validator';
+import { validateIdObl, validateNameObl } from './index.js';
+
+export const validateSpacePost = [
+  ...validateNameObl,
+  check('area')
+    .isFloat()
+    .withMessage('Must be a floating-point number')
+    .notEmpty()
+    .withMessage('Cannot be empty')
+    .bail(),
+  check('buildingId')
+    .isInt()
+    .withMessage('Must be an integer')
+    .notEmpty()
+    .withMessage('Cannot be empty')
+    .bail(),
+  check('spaceTypeId')
+    .isInt()
+    .withMessage('Must be an integer')
+    .notEmpty()
+    .withMessage('Cannot be empty')
+    .bail(),
+  // Add more validation rules for other space properties
+];
+
+export const validateSpacePut = [...validateIdObl, ...validateSpacePost];
