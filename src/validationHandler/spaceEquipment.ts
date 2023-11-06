ac33171bf2d88f4e7fadbea758f97cd77084d09b
nicklasstellberg <nicklas.stellberg@hotmail.com>
2023-11-06 10:26:07 +0200

    Removed file with dublicate code and fixed typo in filename

--- src/validationHandler/spaceEquipment.ts
+++
@@ -1,5 +0,0 @@
-import { check } from 'express-validator';
-import { createIdValidatorChain } from './index.js';
-
-/* ---- SPACEEQUIPMENT ---- */
-export const validateSpaceId = [...createIdValidatorChain('spaceId')];
