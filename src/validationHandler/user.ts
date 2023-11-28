d7b5a684758b99b9e3c68a991f3abaf156ae8bc9
JoonaTeuho <joona.teuho@gmail.com>
2023-11-28 17:09:40 +0200

    fixes

--- src/validationHandler/user.ts
+++ src/validationHandler/user.ts
@@ -9,27 +9,6 @@ export const validateUserId = [...createIdValidatorChain('userId')];
 
 export const validateUserPost = [
   check('email').notEmpty().withMessage('Email cannot be empty').bail(),
-  check('isAdmin')
-    .matches(/[0-1]/)
-    .withMessage('Must be a number between 0 and 1')
-    .bail()
-    .notEmpty()
-    .withMessage('Cannot be empty')
-    .bail(),
-  check('isPlanner')
-    .matches(/[0-1]/)
-    .withMessage('Must be a number between 0 and 1')
-    .bail()
-    .notEmpty()
-    .withMessage('Cannot be empty')
-    .bail(),
-  check('isStatist')
-    .matches(/[0-1]/)
-    .withMessage('Must be a number between 0 and 1')
-    .bail()
-    .notEmpty()
-    .withMessage('Cannot be empty')
-    .bail(),
   ...createRoleValidatorChain('isAdmin'),
   ...createRoleValidatorChain('isPlanner'),
   ...createRoleValidatorChain('isStatist'),
