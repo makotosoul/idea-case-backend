844564069312f478b1244fd366cbad217e492c13
fbiko <fbiko@yahoo.com>
2024-01-25 09:40:06 +0200

    updateKiransChanges-Fred

--- src/routes/departmentplanner.ts
+++ src/routes/departmentplanner.ts
@@ -128,7 +128,12 @@ departmentplanner.put(
         }
       })
       .catch((error) => {
-        dbErrorHandler(req, res, error, 'Error at updating departmentplanner');
+        dbErrorHandler(
+          req,
+          res,
+          error,
+          'Error caat updating departmentplanner',
+        );
       });
   },
 );
