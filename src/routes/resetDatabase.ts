e9980f2890cb142753db55eefefa1d6f2ce458b4
Olli Salo <ullebror@gmail.com>
2024-02-07 09:55:06 +0200

    Removed everything that was unused

--- src/routes/resetDatabase.ts
+++ src/routes/resetDatabase.ts
@@ -1,7 +1,5 @@
 import fs from 'node:fs';
-import dotenv from 'dotenv';
 import express, { Request, Response } from 'express';
-import mysql from 'mysql';
 import { admin } from '../authorization/admin.js';
 import { roleChecker } from '../authorization/roleChecker.js';
 import { authenticator } from '../authorization/userValidation.js';
@@ -13,19 +11,7 @@ import {
 } from '../responseHandler/index.js';
 import { validate } from '../validationHandler/index.js';
 
-dotenv.config({});
-
 const resetDatabase = express.Router();
-
-//creating connection to database and enabling execution of multiple sql statements
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 3306,
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_DATABASE || 'casedb',
-  multipleStatements: true,
-});
 
 //reading sql statements from the file
 const sqlStatements = fs
@@ -35,23 +21,7 @@ const sqlStatements = fs
   )
   .toString();
 
-// //executing the statements through the resetDatabase route with get method using mysql
-/*
-resetDatabase.get(
-  '/',
-  [authenticator, admin, roleChecker, validate],
-  (req: Request, res: Response) => {
-    process.env.BE_DEVELOPMENT_PHASE === 'true'
-      ? connection.query(sqlStatements, (err, results, fields) => {
-          !err
-            ? successHandler(req, res, results, 'Database reset success!')
-            : dbErrorHandler(req, res, err, 'Database reset failed!');
-        })
-      : requestErrorHandler(req, res, 'Not more in development mode!');
-  },
-);
-*/
-
+//resetting the database with knex
 resetDatabase.get(
   '/',
   [authenticator, admin, roleChecker, validate],
@@ -68,17 +38,5 @@ resetDatabase.get(
       : requestErrorHandler(req, res, 'Not in development mode!');
   },
 );
-
-//executing the statements through the resetDatabase route with get method using knex
-// resetDatabase.get(
-//   '/',
-//   [authenticator, admin, roleChecker, validate],
-//   (req: Request, res: Response) => {
-//     knex
-//       .raw(sqlStatements)
-//       .then((data) => successHandler(req, res, data, 'Database reset success!'))
-//       .catch((err) => dbErrorHandler(req, res, err, 'Database reset failed!'));
-//   },
-// );
 
 export default resetDatabase;
