1329aa2b94f1e19888e916e03d9880ddc40cd1f4
Veyefill <nojusklimov@gmail.com>
2024-02-26 03:50:59 +0200

    Finished all backend routing for SpaceType, added a spaceType related request to Spaces.rest

--- src/validationHandler/spaceType.ts
+++ src/validationHandler/spaceType.ts
@@ -1,3 +1,29 @@
-import { createIdValidatorChain } from './index.js';
+import {
+  createIdValidatorChain,
+  validateDescription,
+  validateIdObl,
+  validateMultiDescription,
+  validateMultiNameObl,
+  validateNameObl,
+} from './index.js';
 
 export const validateSpaceTypeId = [...createIdValidatorChain('spaceTypeId')];
+
+export const validateSpaceTypePost = [
+  ...validateNameObl,
+  ...validateDescription,
+];
+
+export const validateSpaceTypePut = [
+  ...validateSpaceTypePost,
+  ...validateIdObl,
+];
+
+//I got the below code from building.ts and it had the following comments on it, so I'm copying them over -Vivienne
+
+// This is a bit different as body can have multiple objects,
+// => MultiPost!!!
+export const validateSpaceTypeMultiPost = [
+  ...validateMultiNameObl,
+  ...validateMultiDescription,
+];
