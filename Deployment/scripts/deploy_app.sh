94eaf86946d16701924a8332baca53f883f39052
hollowdll <pixerchat@gmail.com>
2024-04-08 14:50:34 +0300

    update deployment script

--- Deployment/scripts/deploy_app.sh
+++ Deployment/scripts/deploy_app.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
+pwd=$(pwd)
+
 # Get the latest code changes from git repositories
-cd ~/siba/Siba_be
-git pull
 cd ~/siba/siba-fe
+git pull
+cd ~/siba/Siba_be
 git pull
 
 # Stop and remove current containers
@@ -13,3 +15,5 @@ sudo docker compose -f ~/siba/siba-fe/docker-compose-fe-nginx.yaml down
 # Rebuild the new images and start containers
 sudo docker compose -f ~/siba/Siba_be/docker-compose-dbbe-deploy.yaml up -d --build
 sudo docker compose -f ~/siba/siba-fe/docker-compose-fe-nginx.yaml up -d --build
+
+cd $pwd
