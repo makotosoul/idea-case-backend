16c2a822114ecda6abd96902131bd5949d2c0d42
hollowdll <pixerchat@gmail.com>
2024-04-10 15:37:19 +0300

    update scripts

--- Deployment/scripts/deploy_backend.sh
+++ Deployment/scripts/deploy_backend.sh
@@ -6,4 +6,5 @@ git pull
 
 sudo systemctl start docker
 sudo docker compose -f ~/siba/Siba_be/docker-compose-dbbe-deploy.yaml down
+sudo docker volume rm siba_be_mariadb_data
 sudo docker compose -f ~/siba/Siba_be/docker-compose-dbbe-deploy.yaml up -d --build
--- Deployment/scripts/deploy_whole_app.sh
+++ Deployment/scripts/deploy_whole_app.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-# Get the latest code changes from git repositories
+# Get the latest code changes from the git repositories
 cd ~/siba/siba-fe
 git switch main
 git pull
@@ -8,12 +8,15 @@ cd ~/siba/Siba_be
 git switch main
 git pull
 
-# Start the Docker daemon if it's not running.
+# Start the Docker daemon if it's not running
 sudo systemctl start docker
 
 # Stop and remove current containers
 sudo docker compose -f ~/siba/Siba_be/docker-compose-dbbe-deploy.yaml down
 sudo docker compose -f ~/siba/siba-fe/docker-compose-fe-nginx.yaml down
+
+# Apply new database changes by resetting the database
+sudo docker volume rm siba_be_mariadb_data
 
 # Rebuild the new images and start containers
 sudo docker compose -f ~/siba/Siba_be/docker-compose-dbbe-deploy.yaml up -d --build
