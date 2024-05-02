dabc079d96717fd164252a40bf147bd5e7e50c84
Olli Salo <ullebror@gmail.com>
2024-05-02 09:50:55 +0300

    Added isReadOnly to database scripts

--- Database/SQL_Scripts/06__drop_tables_create_tables_insert_test_data.sql
+++ Database/SQL_Scripts/06__drop_tables_create_tables_insert_test_data.sql
@@ -186,6 +186,7 @@ CREATE TABLE IF NOT EXISTS AllocRound (
     processOn       BOOLEAN     DEFAULT 0,
     abortProcess    BOOLEAN     DEFAULT 0,
     requireReset    BOOLEAN     DEFAULT 0,
+    isReadOnly      BOOLEAN     DEFAULT 0,
 
     PRIMARY KEY(id),
 
