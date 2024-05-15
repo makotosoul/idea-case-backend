2be1649a5078cdefe24ae41214f987381d61485b
Olli Salo <ullebror@gmail.com>
2024-05-15 20:11:15 +0300

    Added test data for isNoisy and isLowNoise

--- Database/SQL_Scripts/01__create_tables.sql
+++ Database/SQL_Scripts/01__create_tables.sql
@@ -263,7 +263,7 @@ CREATE TABLE IF NOT EXISTS AllocSubjectSuitableSpace (
     subjectId       INTEGER     NOT NULL,
     spaceId         INTEGER     NOT NULL,
     missingItems    INTEGER,
-    isLowNoise      BOOLEAN,    NOT NULL,
+    isLowNoise      BOOLEAN    NOT NULL,
 
     PRIMARY KEY(allocRoundId, subjectId, spaceId),
 
